1829f385c482be9c1e49664ead9ea0c3907b602c
misha <slyvkamm@gmail.com>
2019-11-12 14:45:27 +0200

    handle bad parameter

--- elastic/client.go
+++ elastic/client.go
@@ -1,6 +1,7 @@
 package elastic
 
 import (
+	"context"
 	"students_rest_api/config"
 
 	"github.com/olivere/elastic/v7"
@@ -31,18 +32,18 @@ func init() {
 	//		log.WithField("method", "elastic.client.init").Fatal(err)
 	//	}
 	//}
-	//studentsExists, err := client.IndexExists(config.GlobalConfig.StudentsIndex).Do(context.Background())
-	//if err != nil {
-	//	log.WithField("method", "elastic.client.init").Fatal(err)
-	//}
-	//if !studentsExists {
-	//	// need for sorting, if there is no this field in start mapping, app will break down, other part will be created automatically
-	//	mapping := `{"mappings":{"properties":{"id":{"type":"long"}}}}`
-	//	_, err = client.CreateIndex(config.GlobalConfig.StudentsIndex).BodyString(mapping).Do(context.Background())
-	//	if err != nil {
-	//		log.WithField("method", "elastic.client.init").Fatal(err)
-	//	}
-	//}
+	studentsExists, err := client.IndexExists(config.GlobalConfig.StudentsIndex).Do(context.Background())
+	if err != nil {
+		log.WithField("method", "elastic.client.init").Fatal(err)
+	}
+	if !studentsExists {
+		// need for sorting, if there is no this field in start mapping, app will break down, other part will be created automatically
+		mapping := `{"mappings":{"properties":{"id":{"type":"long"}}}}`
+		_, err = client.CreateIndex(config.GlobalConfig.StudentsIndex).BodyString(mapping).Do(context.Background())
+		if err != nil {
+			log.WithField("method", "elastic.client.init").Fatal(err)
+		}
+	}
 
 	log.Info("Connection to ES cluster finished. Address: " + config.GlobalConfig.ElasticAddress)
 }
--- handlers/studentsHandler.go
+++ handlers/studentsHandler.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"encoding/json"
 	"net/http"
+	"regexp"
 	"strings"
 
 	"github.com/sirupsen/logrus"
@@ -49,12 +50,12 @@ func UpdateStudentHandler(w http.ResponseWriter, r *http.Request) {
 		_, _ = w.Write(response.New(1, err.Error(), nil).ToString())
 		return
 	}
-	//if err := elst.New().Body(student).CreateStudent(); err != nil {
-	//	logrus.Errorf("UpdateStudentHandler() CreateStudent() err: %v", err)
-	//	_, _ = w.Write(response.New(1, err.Error(), nil).ToString())
-	//	return
-	//}
-	_, _ = w.Write(response.New(0, "", student).ToString())
+	if err := elst.New().Body(student).UpdateStudent(); err != nil {
+		logrus.Errorf("UpdateStudentHandler() UpdateStudent() err: %v", err)
+		_, _ = w.Write(response.New(1, err.Error(), nil).ToString())
+		return
+	}
+	_, _ = w.Write(response.New(0, "", nil).ToString())
 }
 
 func CreateStudentHandler(w http.ResponseWriter, r *http.Request) {
@@ -83,10 +84,14 @@ func GetStudentHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	_ = r.ParseForm()
 	id := strings.Split(r.URL.String(), "/")[2] // 0 -> "", 1 -> "students", 2 -> {id}
-	jsonData, err := elst.New().Id(convert.Int32(id)).GetStudent().ToJson()
-	if err != nil {
-		_, _ = w.Write(response.New(1, err.Error(), nil).ToString())
+	if re := regexp.MustCompile("[0-9]+"); re.MatchString(id) {
+		jsonData, err := elst.New().Id(convert.Int32(id)).GetStudent().ToJson()
+		if err != nil {
+			_, _ = w.Write(response.New(1, err.Error(), nil).ToString())
+			return
+		}
+		_, _ = w.Write(response.New(0, "", jsonData).ToString())
 		return
 	}
-	_, _ = w.Write(response.New(0, "", jsonData).ToString())
+	_, _ = w.Write(response.New(1, "Bad id of student, please, try another", nil).ToString())
 }
